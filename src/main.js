b801c623aa8f170f0674df5ca3f12061e697c20d
Saklysem <sak.lysem19@kit.edu.kh>
2024-07-24 21:42:50 +0700

    update main

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-// import the fontawesome Icon Library
-import '@fortawesome/fontawesome-free/css/all.css';
-
 createApp(App).mount('#app');
 
