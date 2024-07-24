3987a1624b30fa0e76a94f85623ee6fa13c97757
Saklysem <sak.lysem19@kit.edu.kh>
2024-07-24 21:30:03 +0700

    update main

--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // import the fontawesome Icon Library
 import '@fortawesome/fontawesome-free/css/all.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// The Function to use the Fontawesome icon
 
 createApp(App).mount('#app');
 
