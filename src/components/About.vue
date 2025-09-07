57dcc2caa8db7f30225a480d205fedc83c11f6e0
Saklysem <sak.lysem19@kit.edu.kh>
2025-09-07 23:03:39 +0700

    Update Porfolio Motion Tab

--- src/components/About.vue
+++ src/components/About.vue
@@ -47,7 +47,7 @@ export default{
             </div>
         </div>
         <div class="download">
-            <a href="https://drive.google.com/file/d/160eycU2uF2cMNZaVcBL707GiRfAzqLUN/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/160eycU2uF2cMNZaVcBL707GiRfAzqLUN/view?usp=drive_link">
                 <button id="downloadCV" hreff >Download CV</button>
             </a>
         </div>
